f434df7ebf77a390285490ef4982d1c6a5b795d2
Martijn <martijn@mrtijn.nl>
2019-11-20 12:15:59 +0100

    Add server/exceptions.py

---
+++ server/exceptions.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+    exceptions.py
+    Custom exceptions for the naclpost server
+    This file is part of naclpost.
+
+    Copyright (c) 2019 Martijn
+
+    naclpost is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    naclpost is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with naclpost.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
+class Error(Exception):
+    """Base class for custom exceptions."""
+
+    pass
+
+
+class UnknownPublicKeyError(Error):
+    """Exception raised when a given public key is either not registered with
+    the service, or has a wrong encoding scheme.
+
+    Attributes:
+        pubkey -- the public key that caused the exception to be raised
+    """
+
+    def __init__(self, pubkey):
+        self.pubkey = pubkey
